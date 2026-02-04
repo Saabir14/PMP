23207f2ed035ca8a674f482c66cff560a75885ee
Saabir Ahmed <64490729+Saabir14@users.noreply.github.com>
2026-02-04 14:26:09 +0000

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+// #include <Arduino.h>
+#include <esp_sleep.h>
+#include <LoRa.h>
+#include <RTClib.h>
+// #include "driver/rtc_io.h"
+
+// Sleep Parameters
+#define SLEEP_TIME 5 * 1000000  // Sleep time in microseconds
+#define RTC_ARRAY_SIZE 3
+
+// put function declarations here:
+RTC_DATA_ATTR byte bootCount = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello, World!");
+
+  // Test RTC memory
+  Serial.printf("Boot count: %d\n", bootCount);
+  
+  // Got to sleep
+  # if SLEEP_TIME > 0
+  Serial.printf("Entering deep sleep for %d seconds...\n\n", (int)(SLEEP_TIME / 1e6));
+  esp_sleep_enable_timer_wakeup(SLEEP_TIME);
+  Serial.flush();
+  bootCount++;
+  esp_deep_sleep_start();
+  # endif
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
