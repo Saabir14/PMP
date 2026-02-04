4a3c7016d18caf7c2b75355140695a1768e2ef79
Saabir Ahmed <64490729+Saabir14@users.noreply.github.com>
2026-02-04 15:38:34 +0000

    compiler instructions for deep-sleep when turned off

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,24 @@
 // #include "driver/rtc_io.h"
 
 // Sleep Parameters
-#define SLEEP_TIME 5 * 1000000  // Sleep time in microseconds
+#define SLEEP_TIME 0 * 1000000  // Sleep time in microseconds
 #define RTC_ARRAY_SIZE 3
 
 // put function declarations here:
+# if SLEEP_TIME > 0
 RTC_DATA_ATTR byte bootCount = 0;
+# endif
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Hello, World!");
 
+# if SLEEP_TIME > 0
   // Test RTC memory
   Serial.printf("Boot count: %d\n", bootCount);
   
   // Got to sleep
-# if SLEEP_TIME > 0
   Serial.printf("Entering deep sleep for %d seconds...\n\n", (int)(SLEEP_TIME / 1e6));
   esp_sleep_enable_timer_wakeup(SLEEP_TIME);
   Serial.flush();
