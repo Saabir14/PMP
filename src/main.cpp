c6ef06c134c7e5a3486efc3840bd3461281ed5f4
Saabir Ahmed <64490729+Saabir14@users.noreply.github.com>
2026-02-05 17:18:30 +0000

    Added filtering for temperature sensor using variance

--- src/main.cpp
+++ src/main.cpp
@@ -5,57 +5,117 @@
 // #include "driver/rtc_io.h"
 
 // Sleep Parameters
-// #define SLEEP_ENABLE
-#define SLEEP_TIME 3 * 1000000  // Sleep time in microseconds
+#define SLEEP_ENABLE
+#ifdef SLEEP_ENABLE
+#define SLEEP_TIME 1 * 1000000 // Sleep time in microseconds
 #define RTC_ARRAY_SIZE 3
+RTC_DATA_ATTR byte bootCount = 0;
+#endif
 
-// Temperature Pins
+// Temperature Parameters
 #define TEMP_ENABLE
+#ifdef TEMP_ENABLE
 #define TMEMP_PIN_POS 32
 #define TMEMP_PIN_NEG 33
+#define TEMP_SAMPLES 0xff
+#endif
+
+using namespace std;
 
 // put function declarations here:
-# ifdef SLEEP_ENABLE
-RTC_DATA_ATTR byte bootCount = 0;
-# endif
+int mean(const int *arr, const int size);
+int standard_deviation(const int *arr, const int size);
+int variance(const int *arr, const int size);
+int filter(int *arr, const int size, function<bool(int)> func);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Hello, World!");
 
-# ifdef TEMP_ENABLE
+#ifdef TEMP_ENABLE
   // Read temperature sensor values
-  const int tempPos = analogRead(TMEMP_PIN_POS);
-  const int tempNeg = analogRead(TMEMP_PIN_NEG);
+  int size = TEMP_SAMPLES;
+  int tempReadingsPos[TEMP_SAMPLES], tempReadingsNeg[TEMP_SAMPLES];
+  for (int i = 0; i < TEMP_SAMPLES; i++)
+  {
+    tempReadingsPos[i] = analogRead(TMEMP_PIN_POS);
+    tempReadingsNeg[i] = analogRead(TMEMP_PIN_NEG);
+  }
+
+  const int var = variance(tempReadingsPos, TEMP_SAMPLES);
+  size = filter(tempReadingsPos, size, [var](int x) { return x * x < var * 2; });
+
+  const int tempPos = mean(tempReadingsPos, size);
+  const int tempNeg = mean(tempReadingsNeg, size);
   const int tempDiff = tempPos - tempNeg;
 
   Serial.printf("Temperature Sensor Readings: Positive: %d\n", tempPos);
   Serial.printf("Temperature Sensor Readings: Negative: %d\n", tempNeg);
   Serial.printf("Calculated Sensor Readings: %d\n", tempDiff);
-# endif
+#endif
 
-# ifdef SLEEP_ENABLE
+#ifdef SLEEP_ENABLE
   // Test RTC memory
   Serial.printf("Boot count: %d\n", bootCount);
-  
+
   // Got to sleep
   Serial.printf("Entering deep sleep for %d seconds...\n\n", (int)(SLEEP_TIME / 1e6));
   esp_sleep_enable_timer_wakeup(SLEEP_TIME);
   Serial.flush();
   bootCount++;
   esp_deep_sleep_start();
-# endif
+#endif
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-# ifdef TEMP_ENABLE
-  // Read temperature sensor values
-  const int tempPos = analogRead(TMEMP_PIN_POS);
-  const int tempNeg = analogRead(TMEMP_PIN_NEG);
-  const int tempDiff = tempPos - tempNeg;
+}
 
-  Serial.printf("Temperature Readings: Positive: %d, Negative: %d, Calculated: %d\n", tempPos, tempNeg, tempDiff);
-# endif
+int mean(const int *arr, const int size)
+{
+  int sum = 0;
+  for (int i = 0; i < size; i++)
+  {
+    sum += arr[i];
+  }
+  return sum / size;
+}
+
+int variance(const int *arr, const int size)
+{
+  // Mean of the squares minus the square of the mean
+  int arr_sqr[size];
+  memcpy(arr_sqr, arr, size * sizeof(int));
+  for (int i = 0; i < size; i++)
+  {
+    arr_sqr[i] *= arr_sqr[i];
+  }
+
+  const int m_sqr = mean(arr_sqr, size);
+
+  const int m = mean(arr, size);
+  return m_sqr - m * m;
+}
+
+int standard_deviation(const int *arr, const int size) { return sqrt(variance(arr, size)); }
+
+int filter(int *arr, const int size, function<bool(int)> func)
+{
+  int filtered_size = size;
+  for (int i = 0; i < filtered_size; i++)
+  {
+    if (!func(arr[i]))
+    {
+      // Move this element to the end of the array and decrease the size of the filtered array
+      int temp = arr[i];
+      arr[i] = arr[filtered_size - 1];
+      arr[filtered_size - 1] = temp;
+      filtered_size--;
+      i--; // Check the new element at this index
+    }
+  }
+  return filtered_size;
 }
